162dff97791743d0be3199c4cff2faecf3fe2657
Dima Kruhlyi <dimakruhly@gmail.com>
2019-12-07 23:27:12 +0200

    Task 2 (bit sorting)

--- js/Task_2(30.11.2019)/js/script.js
+++ js/Task_2(30.11.2019)/js/script.js
@@ -1,30 +1,38 @@
-//console.log( Number.parseInt( '1010', 16 ) );
-
-for ( let i=0; i < 15; i++ ) {
-  console.log( `${ i }: ${ (i).toString(2) } `);
-}
-
-( a, b ) => {
-  
-}
-
-// 2**4 = 16 0 - 15
-
-// '1u 2d 0u 3d'
-// u = 0 -> 9
-// d = 9 -> 0
-
-// 0 - 31
-
-// 24: 11000
-// 22: 10110
-// 23: 10111
-
-
-// 12: 1100
-// 13: 1101
-//  8: 1000
-//  9: 1001
-// 14: 1110
-// 10: 1010
-// 11: 1011
+const rand = ( ) => Math.floor( Math.random() * 16 );
+const print = function ( length ) {
+  return function ( value ) {
+    let v = value.toString( 2 );
+    v = '0'.repeat( length-v.length ) + v;
+    console.log(` ${ v } : ${ value } `);
+    return value;
+  };
+};
+const print6 = print( 4 );
+const a = [ ...Array(5) ].map( rand );
+const mask = '1d 0u 3d 2u'
+  .split(' ')
+  .map( function( m, index, A ) {
+    const re = /^(\d+)([ud])$/i;
+    const match = m.match( re );
+    return match ? { index: +match[1], direction: match[2], offset: A.length-index-1, } : false;
+  } )
+  .filter( _ => _ )
+  .sort( ( a, b ) => a.index-b.index );
+const b = a.slice().sort( function( l, r ) {
+  for ( let m of mask ) {
+    const ll = l >> m.offset & 1;
+    const rr = r >> m.offset & 1;
+    if ( ll < rr ) {
+      return m.direction==='d' ? 0 : -1;
+    } else if ( ll > rr ) {
+      return m.direction==='u' ? 0 : -1;
+    }
+  }
+} );
+console.log('1d 0u 3d 2u');
+console.log("a:");
+console.log(a);
+a.forEach( print6 );
+console.log("b:");
+console.log(b);
+b.forEach( print6 );
